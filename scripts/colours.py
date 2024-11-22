695ba3fb3ae0e2e8f34a711a099ce17158491b28
SillyPuffin <bailey.jl.stewart@gmail.com>
2024-11-22 09:55:41 +0000

    gui for sign in

--- scripts/colours.py
+++ scripts/colours.py
@@ -1,3 +1,8 @@
 Heading = "#5BCAA5"
 backgroundColour = "#152434"
 backgroundHighlight = "#253F5B"
+backgroundShadow = "#49473f"
+
+button = '#10d127'
+buttonHover = '#1a7f32'
+buttonPress = ''
--- scripts/login.py
+++ scripts/login.py
@@ -13,17 +13,49 @@ class LoginScreen():
     def createLoginScreen(self):
         self.frame = tk.Frame(master=self.root,background=colours.backgroundColour)
 
-        title = tk.Label(master=self.frame,text="Meme Maker", font =('TkMenuFont',80), fg=colours.Heading, bg = colours.backgroundColour, pady=15)
+        entryWidth = 500
+        entryHeight = 50
+
+        #title
+        title = tk.Label(master=self.frame,text="Meme Maker", font =('impact',80), fg=colours.Heading, bg = colours.backgroundColour, pady=15)
         title.pack(side=tk.TOP)
 
-        loginForm = tk.Frame(master = self.frame,background=colours.backgroundHighlight,border=10,borderwidth=10,highlightcolor=colours.backgroundHighlight)
+        #login text boxes
+        loginForm = ctk.CTkFrame(master = self.frame,border_width=2,border_color="black",corner_radius=6, fg_color=colours.backgroundHighlight)
         loginForm.place(rely=0.5,relx=0.5,anchor='center')
 
-        loginTypelabel = tk.Label(master=loginForm, text='Login', font=("Calibri",40), bg=colours.backgroundColour, fg=colours.Heading, pady=20)
-        loginTypelabel.grid(row=0, column=0, sticky='w')
+        #sign in subheading
+        loginTypelabel = tk.Label(master=loginForm, text='Sign In', font=("Calibri",45), bg=colours.backgroundHighlight, fg=colours.Heading)
+        loginTypelabel.grid(row=0, column=0, sticky='w',padx=10,pady=5)
 
-        user = ctk.CTkEntry(master=loginForm,font=("Microsoft YaHei UI light", 40), height=100, width=500, )
-        user.grid(row=1,column = 0, sticky="w")
+        #divider line
+        divider1 = tk.Frame(master=loginForm, bg=colours.backgroundShadow, height=2 ,width=entryWidth)
+        divider1.grid(column=0,row=1,padx=5,pady=10)
+
+        #entries
+        user = ctk.CTkEntry(master=loginForm,font=("Microsoft YaHei UI light", 30), height=entryHeight, width=entryWidth )
+        user.grid(row=2,column = 0, sticky="S",pady=5,padx=15)
+
+        password = ctk.CTkEntry(master=loginForm,font=("Microsoft YaHei UI light", 30), height=entryHeight, width=entryWidth )
+        password.grid(row=3,column = 0, sticky="N",pady=5,padx=15)
+
+        #second divider line
+        divider2 = tk.Frame(master=loginForm, bg=colours.backgroundShadow, height=2 ,width=entryWidth)
+        divider2.grid(column=0,row=4,padx=5,pady=10)
+
+        #sign in button
+        signIn = ctk.CTkButton(master=loginForm, width=entryWidth, height=50, fg_color=colours.button,hover_color=colours.buttonHover, corner_radius=8, text="Sign In",font=('Calibir',20))
+        signIn.grid(row=5,column=0,pady=10)
+
+        #switch to signup button
+        switchToCreateFrame = tk.Frame(master=loginForm, background=colours.backgroundHighlight)
+        switchToCreateFrame.grid(row=6,column=0,pady=10)
+
+        dontHaveAccount = tk.Label(master=switchToCreateFrame, text="Don't have an Account?",bg=colours.backgroundHighlight, fg='white')
+        dontHaveAccount.pack(side=tk.LEFT)
+
+        signUp = ctk.CTkButton(master=switchToCreateFrame,text='Sign Up?', fg_color='transparent',text_color='#27bee8',width=1,height=1)
+        signUp.pack(side=tk.LEFT)
 
         self.frame.pack(fill=tk.BOTH,side=tk.TOP,expand=True)        
 
